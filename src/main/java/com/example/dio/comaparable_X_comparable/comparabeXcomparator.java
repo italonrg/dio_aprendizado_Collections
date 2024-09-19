71fc69669a9f4a7b4985b42ba88cbc45487ffe1b
italonrg <italonrg500@gmail.com>
2024-09-19 20:09:23 -0300

    first_commit

---
+++ src/main/java/com/example/dio/comaparable_X_comparable/comparabeXcomparator.java
@@ -0,0 +1,14 @@
+package com.example.dio.comaparable_X_comparable;
+
+public class comparabeXcomparator {
+    public static void main(String[] args) {
+        //serve para ordenar basicamente;
+        //comparable fornece uma unica forma de orenação ex : id, preço e tipo ele somente funciona na propria classe;
+        //comparator
+
+    }
+    public class livro {
+        String nome;
+        String ISBN;
+    }
+}
---
+++ src/main/java/com/example/dio/generics/aprendendo_sobregenerics.java
@@ -0,0 +1,9 @@
+package com.example.dio.generics;
+
+public class aprendendo_sobregenerics<T> {
+    //niso eu estou passando um generics com o T, meio que ele e para manter o tipo de dado e de um tipo especifico;
+
+
+    private T t;
+
+}
